85f3b260dcab65ecf5414e4e866ffb54ed217267
Achraf <achraf.zibouhee@gmail.com>
2025-03-18 21:46:52 +0100

    Generics retrieve APIView

--- Users/ASUS/python-dev/drf/backend/products/serializers.py
+++ Users/ASUS/python-dev/drf/backend/products/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Product
 
 class ProducrSerializer(serializers.ModelSerializer):
-  my_discount = serializers.SerializerMethodField(read_only=True)
+#  my_discount = serializers.SerializerMethodField(read_only=True)
   class Meta:
     model = Product
     fields=[
-      'title','content','price','sale_price','my_discount',
+      'title','content','price','sale_price','discount',
     ]
 
   
-    def get_my_discount(self,obj):
-        if not hasattr(obj,'id'):
-           return None
-        if not isinstance():
-           return None
+#    def get_my_discount(self,obj):
+#        if not hasattr(obj,'id'):
+#           return None
+#        if not isinstance():
+#           return None
 
-        return obj.discount()
+#        return obj.discount()
 
--- Users/ASUS/python-dev/drf/backend/products/views.py
+++ Users/ASUS/python-dev/drf/backend/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProducrSerializer
 
 # Create your views here.
+
+class ProductDetailApiView(generics.RetrieveAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProducrSerializer
+
+product_detail_view = ProductDetailApiView.as_view()
