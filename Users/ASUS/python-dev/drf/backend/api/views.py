70cb8218bb85c543e6c97f90b99239d2e1335487
Achraf <achraf.zibouhee@gmail.com>
2025-03-18 18:31:37 +0100

    initial commit

---
+++ Users/ASUS/python-dev/drf/backend/api/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+import json
+from django.forms.models import model_to_dict
+from products.models import Product
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data={}
+    if model_data:
+        
+       # data['title'] = model_data.title
+       # data['content'] = model_data.content
+       # data['price'] = model_data.price
+
+        #is tedious to do each one match to corresponding it stupid use build in function model_to_dict
+        # 
+        data = model_to_dict(model_data,fields=['title'])
+         
+
+    return JsonResponse(data)
